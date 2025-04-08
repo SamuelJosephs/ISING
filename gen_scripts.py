3cecfb5fc3a389694b4e85c5ac163b9bf76d500d
Samuel Josephs <smdjosephs@gmail.com>
2025-04-08 14:08:26 +0100

    Much improved with names less likely to crash SLURM, thanks Vis!

--- gen_scripts.py
+++ gen_scripts.py
@@ -1,38 +1,51 @@
-import numpy as np 
-import subprocess 
+
+import os
+import numpy as np
+import subprocess
 import textwrap
-J_arr = np.linspace(-5,5,10)
-D_arr = np.linspace(-5,5,10)
+
+# Define the directories that need to be created
+directories = ['outfiles', 'errfiles', 'output_dir', 'visualisations']
+
+# Create each directory if it does not already exist
+for d in directories:
+    os.makedirs(d, exist_ok=True)
+
+J_arr = np.linspace(-5, 5, 10)
+D_arr = np.linspace(-5, 5, 10)
 J_prime_arr = np.array([0.0])
 D_prime_array = np.array([0.0])
 B_arr = np.array([1.5])
 time_limit = "00:45:00"
-num_threads=10
-submit = False 
+num_threads = 10
+submit = False
 
+VISSCRIPT = 'skyrmion_evolution_simple.py'
+ISING = 'bin/ISING'
 
 for B in B_arr:
     for J_prime in J_prime_arr:
-        for D_prime in D_prime_array: 
+        for D_prime in D_prime_array:
             for J in J_arr:
                 for D in D_arr:
+                    identifier = f"J_{J:.2f}-Jpr_{J_prime:.2f}-D_{D:.2f}-Dpr_{D_prime:.2f}-B_{B:.2f}"
+                    script = textwrap.dedent(f"""\
+                    #!/bin/bash
+                    #SBATCH --job-name={identifier}          # Job name
+                    #SBATCH --time={time_limit}             # Time limit (hh:mm:ss)
+                    #SBATCH --ntasks=1                  # Number of tasks (1 task = 1 process)
+                    #SBATCH --cpus-per-task={num_threads}           # Number of CPU cores per task (OpenMP threads)
+                    #SBATCH --output=./outfiles/omp_job_{identifier}.out        # Standard output
+                    #SBATCH --error=./errfiles/omp_job_{identifier}.err         # Standard error
+                    #SBATCH -p shared
 
-                    identifier = f"_J={J:.2f}_J'={J_prime:.2f}_D={D:.2f}_D'={D_prime:.2f}_B={B:.2f}_"
-                    script = textwrap.dedent(f"""#!/bin/bash
-#SBATCH --job-name={identifier}          # Job name
-#SBATCH --time={time_limit}             # Time limit (hh:mm:ss)
-#SBATCH --ntasks=1                  # Number of tasks (1 task = 1 process)
-#SBATCH --cpus-per-task={num_threads}           # Number of CPU cores per task (OpenMP threads)
-#SBATCH --output=./outfiles/omp_job_{identifier}.out        # Standard output
-#SBATCH --error=./errfiles/omp_job_{identifier}.err         # Standard error
-#SBATCH -p shared
-
-module load ffmpeg
-# Set the number of OpenMP threads
-export OMP_NUM_THREADS={num_threads}
-./bin/ISING {J} {J_prime} {D} {D_prime} 1.5 ./output_dir_{identifier} && python3 skyrmion_evolution_simple.py ./output_dir_{identifier} ./visualisations {identifier}
+                    module load ffmpeg
+                    # Set the number of OpenMP threads
+                    export OMP_NUM_THREADS={num_threads}
+                    {ISING} {J} {J_prime} {D} {D_prime} 1.5 ./output_dir_{identifier} && python3 {VISSCRIPT} ./output_dir_{identifier} ./visualisations {identifier}
                     """)
-                    with open(f"script_{identifier}.sh", "w") as f:
+                    script_filename = f"script_{identifier}.sh"
+                    with open(script_filename, "w") as f:
                         f.write(script)
                     if submit:
-                        result = subprocess.run([f"sbatch",f"script_{identifier}.sh"])
+                        subprocess.run(["sbatch", script_filename])
