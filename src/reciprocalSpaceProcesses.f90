f533bb996222a12db6effe22c87268eebc490695
Samuel Josephs <smdjosephs@gmail.com>
2025-04-22 14:57:47 +0100

    Finished first attempt at cloud in cell interpolation for an FFT grif

--- src/reciprocalSpaceProcesses.f90
+++ src/reciprocalSpaceProcesses.f90
@@ -20,12 +20,12 @@ module reciprocal_space_processes
                 chainMesh%fft_array_x = real(0.0,C_DOUBLE)
                 chainMesh%fft_array_y = real(0.0,C_DOUBLE) 
                 chainMesh%fft_array_z = real(0.0,C_DOUBLE)
-                if (.not. allocated(chainMesh%derivativeList)) error stop "Derivative List not allocated"
-                if (.not. all([N,L,M] == shape(chainMesh%DerivativeList))) error stop "Derivative List is the wrong shape"
-                if (stat /= 0) error stop "Allocation Failed"
                 do CellIndex = 1,chainMesh%numChainMeshCells
                         call getNeighboringCells(chainMesh,CellIndex,nearestNeighborCellList)
                         call coordinatesFromIndex(chainMesh,CellIndeX, iCell, jCell, kCell)
+                        iCell = iCell + 1 ! coordinatesFromIndex works for 0 based indexing 
+                        jCell = jCell + 1 
+                        kCell = kCell + 1 
                         x = chainMesh%chainMeshCells(CellIndex)%centreX
                         y = chainMesh%chainMeshCells(cellIndex)%centreY
                         z = chainMesh%chainMeshCells(cellIndex)%centreZ
@@ -59,6 +59,59 @@ module reciprocal_space_processes
                 end do 
         end subroutine interpolate_to_fft_array 
         
+        subroutine interpolate_fft_to_atoms(chainMesh, outputArray)
+                implicit none 
+                type(chainMesh_t), intent(inout) :: chainMesh 
+                real(kind=8), allocatable, dimension(:,:), intent(out) :: outputArray ! i,j in [1,size(chainmesh%atoms)], [1,3]
+                integer :: atom, iCell, jCell, kCell, chainCellIndex, atomIndex, N,L,M , stat, cellIndex, cellIndexTemp  
+                integer, dimension(27) :: nearestNeighborCellList
+                type(vecNd_t) :: cellCentre, atomPos, d 
+                real(kind = 8) :: x,y,z, xWeight, yWeight, zWeight, cellWidth, weight  
+                cellWidth = dble(chainMesh%latticeParameter)
+                N = chainMesh%numCellsX 
+                L = chainMesh%numCellsY 
+                M = chainMesh%numCellsZ 
+                allocate(outputArray(size(chainMesh%atoms),3), stat=stat) 
+                outputArray = 0.0_8
+                if (stat /= 0) error stop "failed to allocate output array"
+                do CellIndex = 1,chainMesh%numChainMeshCells
+                        call getNeighboringCells(chainMesh,CellIndex,nearestNeighborCellList)
+                        call coordinatesFromIndex(chainMesh,CellIndeX, iCell, jCell, kCell)
+                        iCell = iCell + 1 ! coordinatesFromIndex works with 0 based indexing 
+                        jCell = jCell + 1 
+                        kCell = kCell + 1 
+                        x = chainMesh%chainMeshCells(CellIndex)%centreX
+                        y = chainMesh%chainMeshCells(cellIndex)%centreY
+                        z = chainMesh%chainMeshCells(cellIndex)%centreZ
+                        cellCentre = makeVecNdCheck(cellCentre,[x,y,z])
+                        do cellIndexTemp = 1,size(nearestNeighborCellList)
+                               atom = chainMesh%chainMeshCells(nearestNeighborCellList(cellIndexTemp))%firstAtomInMeshCell
+                               do while (atom /= -1)
+                                   x = dble(chainMesh%atoms(atom)%x) 
+                                   y = dble(chainMesh%atoms(atom)%y)
+                                   z = dble(chainMesh%atoms(atom)%z) 
+                                   atomPos = makeVecNdCheck(atomPos,[x,y,z])
+                                   call distance_points_vec(chainMesh,cellCentre,atomPos,d) !Takes periodic boundaries into account.
+                                   
+                                   !Now calculate the cloud in cell interpolation weights 
+
+                                   xWeight = 1.0_8 - (abs(d%coords(1))/cellWidth)
+                                   yWeight = 1.0_8 - (abs(d%coords(2))/cellWidth) 
+                                   zWeight = 1.0_8 - (abs(d%coords(3))/cellWidth)
+                                   weight = xWeight*yWeight*zWeight
+                                        
+                                   outputArray(atom,1) = outputArray(atom,1) +  weight*chainMesh%fft_array_x(iCell,jCell,kCell)
+                                   outputArray(atom,2) = outputArray(atom,2) +  weight*chainMesh%fft_array_y(iCell,jCell,kCell)
+                                   outputArray(atom,3) = outputArray(atom,3) +  weight*chainMesh%fft_array_z(iCell,jCell,kCell)
+                                   atom = chainMesh%atoms(atom)%nextAtom 
+                               end do 
+                        end do 
+                        
+                end do        
+
+        end subroutine interpolate_fft_to_atoms 
+
+
         subroutine fft_forward_chainMesh(chainMesh)
                 type(chainMesh_t), intent(inout) :: chainMesh 
 
