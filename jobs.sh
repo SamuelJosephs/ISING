8a6da12072ea5545939cc7037016c2f2d167fb9f
SamuelJosephs <82320097+SamuelJosephs@users.noreply.github.com>
2025-07-03 16:41:32 +0100

    Update jobs.sh
    Changed file input parameters

--- jobs.sh
+++ jobs.sh
@@ -1,54 +1,79 @@
-#!/usr/bin/bash
+import os
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
 
-maxJ=1.2
-minJ=0.0
-NJ=20
 
-maxD=3.5
-minD=0.0
-ND=50
+directory_path = "./output-dir/"
+dfs = []
+with os.scandir(directory_path) as it: 
+    for entry in it:
+        if entry.is_file():
+            filename = entry.name
+            df = pd.read_csv(directory_path + filename)
+            dfs.append(df)
+df = pd.concat(dfs,ignore_index = True)
+df.columns = df.columns.str.strip()
+print(df)
 
-maxB=1.5
-minB=1.5
-NB=1
+df_min_T = df[df["T"] == min(df["T"])]
 
-outputDir="./output-dir" # prefix for data directories
-outfilesDir="./outfiles" # Teh directory where stdout will be directed to
-maxTime="3:20:00"
-maxConcurrentJobs=500
-###########################################################################################
+J_vals_min_T = df_min_T["J"].to_numpy()
+D_vals_min_T = df_min_T["D"].to_numpy()
+B_vals_min_T = df_min_T["B"].to_numpy
+mz_vals_min_T = df_min_T["sz"].to_numpy()
+winding_middle_vals_min_T = df_min_T["winding_number_middle"].to_numpy()
 
-module load python openmpi fftw
-N=$((NJ*ND*NB - 1)) # Indiced through 0 to N-1
+skyrmion_number_min_T = df_min_T["skyrmion_number_middle"].to_numpy()
+skyrmion_spread_min_T = df_min_T["winding_number_spread"].to_numpy()
 
-mkdir -p $outfilesDir
 
-script=$(cat <<EOF
-#!/usr/bin/bash
-#SBATCH -p shared
-#SBATCH -n 1 
-#SBATCH -c 1 
-#SBATCH -J parameter-scan
-#SBATCH -t ${maxTime}
-#SBATCH -o ${outfilesDir}/%a.out 
-NJ=${NJ}
-ND=${ND}
-NB=${NB}
-N=${N}
-index=\${SLURM_ARRAY_TASK_ID}
+# Filter for values close to 
+skyrmion_number_min_T = np.where(abs(skyrmion_spread_min_T) < 2.1,
+                                        skyrmion_number_min_T,np.zeros(np.shape(skyrmion_number_min_T)))
+J_values_unique = np.unique(J_vals_min_T)
+D_vals_unique = np.unique(D_vals_min_T)
 
-i=\$(python -c "import math; x = int(math.floor(\${index}/(\${NB}*\${ND}))); print(x)")
-rem=\$(python -c "x = int(\${index}) % int(\${NB}*\${ND}); print(x)")
-j=\$(python -c "import math; x = int(math.floor(\${rem} / int(\${NB}))); print(x)")
-k=\$(python -c "x = int(\${rem}) % int(\${NB}); print(x)")
+winding_matrix = np.zeros((len(D_vals_unique),len(J_values_unique)))
+mz_matrix = np.zeros((len(D_vals_unique),len(J_values_unique)))
+for i in range(0,len(skyrmion_number_min_T)):
+    D_val = D_vals_min_T[i]
+    J_val = J_vals_min_T[i]
+    J_Index = np.nonzero(J_values_unique == J_val)[0]
+    D_Index = np.nonzero(D_vals_unique == D_val)[0]
+    winding_matrix[D_Index,J_Index] = skyrmion_number_min_T[i]
+    mz_matrix[D_Index,J_Index] = mz_vals_min_T[i]
 
-Jval=\$(python -c "x = (\${i}/\${NJ})*(${maxJ} - ${minJ}) + ${minJ}; print(x)")
-Dval=\$(python -c "x = (\${j}/\${ND})*(${maxD} - ${minD}) + ${minD}; print(x)")
-Bval=\$(python -c "x = (\${k}/\${NB})*(${maxB} - ${minB}) + ${minB}; print(x)")
 
-mpirun -np 1 ./bin/PT \${Jval} \${Jval} 1 \${Dval} \${Dval} 1 \${Bval} \${Bval} 1 ${outputDir} 
-EOF
-)
 
-echo "${script}" | sbatch --array=0-${N}%${maxConcurrentJobs}  
+fig_sk_hm, axes_sk_hm = plt.subplots()
+fig_mz_hm, axes_mz_hm = plt.subplots()
 
+z = axes_sk_hm.pcolormesh(
+        J_values_unique,
+        D_vals_unique,
+        winding_matrix,
+        shading="auto",
+        cmap="inferno"
+        )
+l = axes_mz_hm.pcolormesh(J_values_unique,
+                          D_vals_unique,
+                          mz_matrix,
+                          shading="auto",
+                          cmap="inferno")
+axes_sk_hm.set_xlabel("J")
+axes_sk_hm.set_ylabel("D")
+axes_mz_hm.set_xlabel("J")
+axes_mz_hm.set_ylabel("D")
+
+
+cbar_sk_hm = fig_sk_hm.colorbar(z,ax=axes_sk_hm)
+cbar_sk_hm.set_label("Skyrmion Number")
+cbar_mz_hm = fig_mz_hm.colorbar(l,ax=axes_mz_hm)
+cbar_mz_hm.set_label("mz")
+
+fig_sk_hm.savefig("./sk_hm.pdf",bbox_inches="tight")
+fig_mz_hm.savefig("./mz_hm.pdf",bbox_inches="tight")
+
+
+plt.show()
