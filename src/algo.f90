743b0829f3314263d4959d1824f63a3352439d85
Samuel Josephs <samuel.josephs@chicagoclubteam.org>
2025-09-05 18:20:18 +0100

    Fixed quicksort

--- src/algo.f90
+++ src/algo.f90
@@ -16,62 +16,79 @@ contains
                 real(kind=dp), dimension(:), intent(inout) :: array 
                 integer, dimension(:), optional, intent(inout) :: integer_companion
                 integer :: lesserPartitionIndex, GreaterPartitionIndex, &
-                        pivotIndex, pivot  
-                real(kind=dp) :: temp
-
-                if (present(integer_companion)) then 
-                        if (size(integer_companion) /= size(array)) error stop "Error: &
-                                integer_companion must be the same size as array"
-                end if 
-                if (size(array) == 1) return 
-                if (size(array) == 2) then 
-                        if (array(1) <= array(2)) return 
-                end if 
-                ! Choose pivot to be in the middle of the array 
-                pivotIndex = size(array)/2 
+                        pivotIndex  
+                real(kind=dp) :: temp, pivot
+                integer :: temp_int 
                 
-                lesserPartitionIndex = 1
-                GreaterPartitionIndex = size(array) 
+                if (size(array) == 1) return 
 
+
+                print *, "entering quicksort with array :", array
+                ! Choose pivot 
+
+                pivotIndex = size(array) / 2
                 pivot = array(pivotIndex)
 
-                do while (lesserPartitionIndex < GreaterPartitionIndex)
-                        
-                        if (array(lesserPartitionIndex) > pivot) then 
-                                temp = array(lesserPartitionIndex)
-                                array(lesserPartitionIndex) = pivot 
-                                array(pivotIndex) = temp 
-                                if (present(integer_companion)) then 
-                                        temp = integer_companion(lesserPartitionIndex)
-                                        integer_companion(lesserPartitionIndex) = integer_companion(pivotIndex) 
-                                        integer_companion(pivotIndex) = temp 
-                                end if 
-                                pivotIndex = lesserPartitionIndex 
-                        end if 
-                        if (array(GreaterPartitionIndex) < pivot) then 
-                                temp = array(GreaterPartitionIndex)
-                                array(GreaterPartitionIndex) = pivot 
-                                array(pivotIndex) = temp 
-                                if (present(integer_companion)) then 
-                                        temp = integer_companion(GreaterPartitionIndex)
-                                        integer_companion(GreaterPartitionIndex) = integer_companion(pivotIndex) 
-                                        integer_companion(pivotIndex) = temp 
-                                end if 
-                                pivotIndex = GreaterPartitionIndex 
-                        end if 
-                
-                        lesserPartitionIndex = lesserPartitionIndex + 1
-                        GreaterPartitionIndex = GreaterPartitionIndex - 1
-                end do 
+                ! Now partition into elements below element and above element 
 
-                if (.not. present(integer_companion)) then  
-                        call quicksort_dp(array(1:GreaterPartitionIndex))
-                        call quicksort_dp(array(GreaterPartitionIndex + 1:size(array))) 
+                lesserPartitionIndex = 1
+                GreaterPartitionIndex = size(array)
+
+                do while (lesserPartitionIndex < GreaterPartitionIndex) 
+                        ! Move lesser index until we find element that shouldn't be in the lesser partition 
+                        ! Same for the right hand partition
+                        if (array(lesserPartitionIndex) < pivot) lesserPartitionIndex = lesserPartitionIndex + 1 
+                        if (array(GreaterPartitionIndex) > pivot) GreaterPartitionIndex = GreaterPartitionIndex - 1 
+
+                        ! Swap if suitable 
+
+                        if ((array(lesserPartitionIndex) >= pivot) .and. (array(GreaterPartitionIndex) <= pivot)) then 
+                                if (lesserPartitionIndex == pivotIndex) then 
+                                        pivotIndex = GreaterPartitionIndex
+                                else if (GreaterPartitionIndex == pivotIndex) then 
+                                        pivotIndex = lesserPartitionIndex 
+                                end if 
+                                call swap_dp_array_elements(array,lesserPartitionIndex,GreaterPartitionIndex)
+                                if (present(integer_companion)) call swap_integer_array_elements(integer_companion,&
+                                                                        lesserPartitionIndex, GreaterPartitionIndex)
+                        end if 
+
+                end do 
+                if (pivotIndex == size(array)) pivotIndex = pivotIndex - 1
+                
+                if (.not. present(integer_companion)) then 
+                        call quicksort_dp(array(1:pivotIndex))
+                        call quicksort_dp(array(pivotIndex + 1:size(array)))
                 else 
-                        call quicksort_dp(array(1:GreaterPartitionIndex),&
-                                integer_companion=integer_companion(1:GreaterPartitionIndex))
-                        call quicksort_dp(array(GreaterPartitionIndex + 1:size(array)),&
-                                integer_companion=integer_companion(GreaterPartitionIndex + 1:size(integer_companion))) 
+                        call quicksort_dp(array(1:pivotIndex),integer_companion=integer_companion(1:pivotIndex))
+                        call quicksort_dp(&
+                                array(pivotIndex + 1:size(array)),&
+                                integer_companion=integer_companion(pivotIndex + 1:size(array)))
                 end if 
         end subroutine quicksort_dp 
+
+        subroutine swap_dp_array_elements(array,i,j)
+                use iso_fortran_env, only: dp=>real64 
+                implicit none 
+                real(kind=dp), dimension(:), intent(inout) :: array 
+                integer, intent(in) :: i,j
+                real(kind=dp) :: temp 
+
+                temp = array(i) 
+                array(i) = array(j) 
+                array(j) = temp 
+                return 
+        end subroutine swap_dp_array_elements 
+
+        subroutine swap_integer_array_elements(array,i,j)
+                implicit none 
+                integer, dimension(:), intent(inout) :: array 
+                integer, intent(in) :: i,j
+                integer :: temp 
+
+                temp = array(i) 
+                array(i) = array(j) 
+                array(j) = temp 
+                return 
+        end subroutine swap_integer_array_elements 
 end module algo 
