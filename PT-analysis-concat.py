b7c1cb5b5ba12fc2bdfb0c448bc6b38aa489c207
JOSEPHS <tnjq27@login1.ham8.dur.ac.uk>
2025-11-05 16:12:11 +0000

    Working plotting code without artifacts

--- PT-analysis-concat.py
+++ PT-analysis-concat.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import SymLogNorm
 import numpy as np 
 import pandas as pd
+import os
 
 dataPath = "./concatenated_data.csv"
+wd = os.getcwd()
+outputPath = os.path.join(wd,"PT-analysis-concat-plots")
+os.makedirs(outputPath,exist_ok = True)
+
+
 
 df = pd.read_csv(dataPath,na_values="********")
 df = df.fillna(-1)
@@ -45,6 +51,33 @@ buffShape = (len(BVals_unique),
 
 heatMapBuffers = np.zeros(buffShape)
 
+
+
+def plot_image(imageBuff, filename, origin: str = "lower", titleString: str = None, logCmap = False):
+    import matplotlib.pyplot as plt 
+    from matplotlib.colors import SymLogNorm
+    fig, ax = plt.subplots()
+    
+    if logCmap:
+        vmax = np.abs(imageBuff).max()
+        norm = SymLogNorm(linthresh = 0.1*vmax)
+
+        mappable = ax.imshow(imageBuff, origin = origin, norm = norm)
+    cbar = plt.colorbar(mappable, ax = ax)
+    
+    ax.set_xlabel("J (ev)")
+    ax.set_ylabel("D (ev)")
+    fig.savefig(filename,bbox_inches = "tight")
+    
+    titleString = ""
+    if titleString != None:
+        ax.set_title(titleString)
+
+    
+    ax.clear()
+    fig.clf()
+
+
 for t,TVal in enumerate(TVals_unique):
     for b, BVal in enumerate(BVals_unique):
         mask = (dataBuffer[0] == BVal) & (dataBuffer[1] == TVal)
@@ -61,13 +94,34 @@ for t,TVal in enumerate(TVals_unique):
         sk_num_plot = dataArray[2]
         wnd_num_plot = dataArray[3]
 
+        sk_num_image = np.zeros((len(JVals_unique),len(DVals_unique)))
+        wnd_num_image = np.zeros((len(JVals_unique),len(DVals_unique)))
+        
+        for j, JVal in enumerate(JVals_unique):
+            Jindices, = np.nonzero(JVals_plot == JVal) # Now that we know the indices of all values with a specific Jval we need to figure out where these are for each D value.
+            DVals_Jindices = DVals_plot[Jindices] # Now we have all of the values along one J column we need to sort by D so that the elements are in the right order
+            sortedD_indices = np.argsort(DVals_Jindices)
 
+            sorted_Jindices = Jindices[sortedD_indices]
+
+            sk_num_Js = sk_num_plot[sorted_Jindices] # Skyrmion Numbers for the specific J value
+            sk_num_image[j,:] = sk_num_Js
+
+            wnd_num_Js = wnd_num_plot[sorted_Jindices]
+            wnd_num_image[j,:] = wnd_num_Js
+                 
+         
         # Replace JVals_plot, a length NJxND one dimensional array with a list of indices each element maps to.
+        filename_sk_num = os.path.join(wd,f"PT-analysis-concat-plots/sk_num_{t}_{b}.pdf")
+        filename_wnd_num = os.path.join(wd,f"PT-analysis-concat-plots/wnd_num_{t}_{b}.pdf")
 
+        skNum_titleString = "Skyrmion Number for $\\mu_b B$ = {Bval:.3f}ev, T = {TVal}K"
+        wndNum_titleString = "Winding Number for $\\mu_b B$ = {Bval:.3f}ev, T = {TVal}K"
+        plot_image(sk_num_image,filename_sk_num, titleString = skNum_titleString, logCmap = True)
+        plot_image(wnd_num_image,filename_wnd_num, titleString = wndNum_titleString, logCmap = True)
         # Use np.where with broadcasting.
         print(f"Shape of dataArray = {np.shape(dataArray)}")
 
-        fig, ax = plt.subplots()
-
-        ax.imshow()
+        
+        
 
