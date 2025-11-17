4d7f46adb9199003b60b3692ddad65d393a4c4a9
JOSEPHS <tnjq27@login1.ham8.dur.ac.uk>
2025-11-17 15:15:08 +0000

    Initial in place transform code

--- src/fft.f90
+++ src/fft.f90
@@ -3,16 +3,27 @@ module fft
 use iso_c_binding
 include 'fftw3.f03' 
 
+interface fft_2d_inplace 
+       module procedure fft_2d_inplace_rBuff
+       module procedure fft_2d_inplace_cBuff
+end interface fft_2d_inplace 
+
+
 type fft_object
         type(C_ptr) :: plan_forward, plan_backward
         real(kind=c_double), pointer :: fft_array 
-        type(C_ptr) :: fft_array_base_ptr ! Points to the first element of the array 
-        integer :: buffer_len ! Length of the array buffer, proper C programming in Fortran I'm sure there is no risk of stack
-                              ! smashing myself again.
-
+        type(C_ptr) :: fft_array_real_base_ptr ! Points to the first element of the real space array 
+        type(C_ptr) :: fft_array_recip_base_ptr ! Points to the first element of the reciprocal space array 
+        integer :: real_buffer_len ! Length of the array buffer, proper C programming in Fortran I'm sure there is no risk of stack
+                                   ! smashing myself again.
+        integer :: recip_buffer_len
+        integer, dimension(:), allocatable :: RealBufferShape  ! (shape of the real space array)
+        integer, dimension(:), allocatable :: RecipBufferShape ! (shape of the reciprocal space array, can differ from that of the
+                                                               ! real space shape for an in place transform)
 
 end type fft_object
 
+public :: fft_2d_inplace
 
 contains 
 
@@ -26,30 +37,105 @@ contains
                 complex(kind=c_double_complex), intent(out), pointer, dimension(:,:) :: outputBufferComplex
 
                 integer(kind=c_size_t) :: Nx_c, Ny_c
-                
+                integer :: stat 
                 Nx_c = int(Nx,c_size_t)
                 Ny_c = int(Ny,c_size_t)
 
-                ! We only need Nx/2 complex numbers in the transformed complex space, then add 1 and multiply by 2 to make sure
-                ! there is enough space for the transformed buffer
-                fft_obj%fft_array_base_ptr = fftw_alloc_real((2*(Nx_c/2 + 1))*Ny_c) 
-                                                                                               
-                call C_F_POINTER(fft_obj%fft_array_base_ptr,outputBufferReal,[(2*(Nx/2 + 1)),Ny])
+                ! We only need Nx/2 +1 complex numbers in the transformed, +1 for the 0 frequency, then N up to the Nyquist
+                ! frequency.
+                ! complex space due to symmetry above the Nyquist frequency FFTW reuses these values for lower memory
+                ! overhead and faster FFT's. 
 
-                call C_F_POINTER(fft_obj%fft_array_base_ptr,outputBufferComplex,[Nx,Ny])
+                ! Periodicity requires that in reciprocal space every N entries are identical. Thus f^twiddle(N-k) = f(k)^*
+                ! For frequencies abovet the Nyquist frequency this maps to  
+                ! Note: This is only true for real valued funcitons, where f = f^*
+                fft_obj%fft_array_real_base_ptr = fftw_alloc_real((2*(Nx_c/2 + 1))*Ny_c) 
+                fft_obj%fft_array_recip_base_ptr = fft_obj%fft_array_real_base_ptr
+                                                                                               
+                call C_F_POINTER(fft_obj%fft_array_real_base_ptr,outputBufferReal,[Nx,Ny])
+
+                call C_F_POINTER(fft_obj%fft_array_recip_base_ptr,outputBufferComplex,[Nx/2 + 1,Ny])
                  ! Now outputBuffer will behave like an ordinary 2d array, 
                 outputBufferReal(:,:) = 0.0_c_double ! Zero the array to avoid uninitialised data later.
 
-                fft_obj%buffer_len = product(shape(outputBufferReal))
+                fft_obj%real_buffer_len = product(shape(outputBufferReal))
+                fft_obj%recip_buffer_len = product(shape(outputBufferComplex))
+
+                allocate(fft_obj%RealBufferShape(2),stat=stat)
+                if (stat /= 0) error stop "Error: Failed to allocate real buffer shape"
+
+                allocate(fft_obj%RecipBufferShape(2),stat=stat)
+                if (stat /= 0) error stop "Error: Failed to allocate reciprocal buffer shape"
+
+                fft_obj%RealBufferShape = shape(outputBufferReal)
+                fft_obj%RecipBufferShape = shape(outputBufferComplex)
 
                 ! Now we need to initialise the plans 
 
+                ! We have to transpose the shapes in the actual C calls.
                 fft_obj%plan_forward = fftw_plan_dft_r2c_2d(Ny,Nx,outputBufferReal,outputBufferComplex,FFTW_ESTIMATE)
                 fft_obj%plan_backward = fftw_plan_dft_c2r_2d(Ny,Nx,outputBufferComplex,outputBufferReal,FFTW_ESTIMATE)
                 
 
 
         end subroutine create_plan_2d_inplace
+
+
+        subroutine fft_2d_inplace_rBuff(fft_obj,BufferReal,direction)
+                ! This function assumes that the real and complex buffers have been initialised with create_plan_2d_inplace, not
+                ! doing this could very easily cause a book keeping error!!!!!!
+                type(fft_object), intent(in) :: fft_obj
+                real(kind=c_double), intent(inout), target, dimension(:,:) :: BufferReal
+                character(len=1), intent(in) :: direction ! F = Forward, B = Backwards
+                complex(kind=c_double_complex),  pointer, dimension(:,:) :: BufferComplex
+                type(C_ptr) :: ptr
+                integer, dimension(2) :: bufferShape
+                integer :: p
+
+
+                bufferShape = shape(BufferReal)
+                ptr = C_loc(BufferReal)
+                call C_F_POINTER(ptr,BufferComplex,[bufferShape(1)/2 + 1,bufferShape(2)]) 
+                if (direction == 'F') then 
+                        call fftw_execute_dft_r2c(fft_obj%plan_forward,BufferReal,BufferComplex)
+                else if (direction == 'B') then 
+                        call fftw_execute_dft_c2r(fft_obj%plan_backward,BufferComplex,BufferReal)
+                else 
+                        error stop "Error: Invalid option passed to fft_2d_realBuff"
+                end if 
+                
+        end subroutine fft_2d_inplace_rBuff
+
+
+        subroutine fft_2d_inplace_cBuff(fft_obj,BufferComplex,direction)
+                ! This function assumes that the real and complex buffers have been initialised with create_plan_2d_inplace, not
+                ! doing this could very easily cause a book keeping error!!!!!!
+                type(fft_object), intent(in) :: fft_obj
+                complex(kind=c_double_complex), intent(inout), target, dimension(:,:) :: BufferComplex
+                character(len=1), intent(in) :: direction ! F = Forward, B = Backwards
+                real(kind=c_double), pointer, dimension(:,:) :: BufferReal
+                type(C_ptr) :: ptr
+                integer, dimension(2) :: bufferShape
+                
+
+                bufferShape = shape(BufferComplex)
+
+
+
+                ptr = C_loc(BufferComplex)
+                ! N_complex = N_real / 2 + 1 -> N_real =2* N_complex - 2
+                call C_F_POINTER(ptr,BufferReal,[bufferShape(1)*2 - 2,bufferShape(2)]) 
+                if (direction == 'F') then 
+                        call fftw_execute_dft_r2c(fft_obj%plan_forward,BufferReal,BufferComplex)
+                else if (direction == 'B') then 
+                        call fftw_execute_dft_c2r(fft_obj%plan_backward,BufferComplex,BufferReal)
+                else 
+                        error stop "Error: Invalid option passed to fft_2d_realBuff"
+                end if 
+                
+        end subroutine fft_2d_inplace_cBuff
+
+
 
 
 end module fft
